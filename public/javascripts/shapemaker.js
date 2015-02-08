7815cb0a2c32bd29efb9572b52652d73eeec698d
bosonX <xijin1991@gmail.com>
2015-02-08 14:28:07 -0500

    changed hexmaker function to HexContainer constructor for clearness,中文测试

--- public/javascripts/shapemaker.js
+++ public/javascripts/shapemaker.js
@@ -1,16 +1,37 @@
-function hexmaker(radius,hex_length){
+//class of Hex
+function HexContainer(radius,hex_length){
+	var root_3 = Math.pow(3,0.5);
+	this.radius = radius;
+	this.hex_length = hex_length;
+	this.svg_width = this.radius * 2 * (this.hex_length * 2 - 1);
+	this.svg_height = (this.svg_width - 2 * this.radius) * root_3 / 2 + 2 * this.radius;
+	this.total_units = this.calculateTotalUnits(this.hex_length);
+	this.row_lengths = this.calculateRowLengths(this.hex_length);
+	this.circles = this.calculateAllCircles(this.radius, this.hex_length);
+}
+
+HexContainer.prototype.calculateTotalUnits = function(hex_length){
+	return 6 * (hex_length) * (hex_length - 1) / 2 + 1;
+}
+
+HexContainer.prototype.calculateRowLengths = function(hex_length){
 	var middle_length = hex_length * 2 - 1;
 	var row_lengths = [];
-	var root_3 = Math.pow(3,0.5);
 	for(var i=hex_length; i<=middle_length; i++){
 		row_lengths.push(i);
 	}
 	for(var i = middle_length - 1; i >= hex_length; i--){
 		row_lengths.push(i);
 	}
+	return row_lengths;
+}
+
+HexContainer.prototype.calculateAllCircles = function(radius,hex_length){
+	var root_3 = Math.pow(3,0.5);
+	var middle_length = hex_length * 2 - 1;
 	var circles = [];
 	for(var y= 0; y < middle_length; y++){
-		for(var x=0; x<row_lengths[y]; x++){
+		for(var x=0; x<this.row_lengths[y]; x++){
 			var current = {};
 			current.cx = radius * x * 2 + radius + Math.abs(y + 1 - hex_length) * radius;
 			current.cy = radius * y * root_3 + radius;
@@ -18,17 +39,7 @@ function hexmaker(radius,hex_length){
 			circles.push(current);
 		}
 	}
-
-	var svg_height = radius * 2 * (hex_length * 2 + 1);
-	var svg_width = svg_height;
-
-	var total_units = 1;
-	for(var i = 0 ; i < hex_length; i++){
-		total_units += 6 * i;
-	}
-	
-	
-	return {total_units:total_units , svg_width : svg_width, svg_height : svg_height, radius : radius, circles : circles};
+	return circles;
 }
 
-module.exports.hexmaker = hexmaker;
+module.exports.HexContainer = HexContainer;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-var hexmaker = require('../public/javascripts/shapemaker.js').hexmaker;
+var HexContainer = require('../public/javascripts/shapemaker.js').HexContainer;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', hexmaker(5,30));
-  // res.json(hexmaker(100,3));
+  res.render('index', new HexContainer(1,30));
 });
 
 module.exports = router;
